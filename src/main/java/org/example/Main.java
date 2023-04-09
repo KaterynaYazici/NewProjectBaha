d632b99dc27034303fe49526980000e64efeb77f
KaterynaYazici <katerynayazici7@gmail.com>
2023-04-09 07:12:36 -0700

    branch2 - commit3 -total6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         test_2.println();
+        test_2.println();
+        test_2.println();
     }
 }
